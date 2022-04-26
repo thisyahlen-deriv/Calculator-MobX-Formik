b22f03121b25031313ded0c735de0be632c3a69b
thisyahlen-deriv <thisyahlen@regentmarkets.com>
2022-04-26 10:28:15 +0800

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
 import './App.css'
-import { observer, inject } from 'mobx-react'
-import { buttons } from './btnConfig'
+import Form from './form'
 
-const App = ({ store }) => {
-  return (
-    <>
-      <div className="container">
-        <form>
-          <input
-            type="text"
-            value={store.inputVal}
-            onChange={(e) => e.target.value}
-          />
-        </form>
-        <form>
-          <input
-            type="text"
-            value={store.result}
-            onChange={(e) => e.target.value}
-          />
-        </form>
-
-        <div className="keypad">
-          <button className="highlight" onClick={store.clear} id="clear">
-            Clear
-          </button>
-          <button
-            className="highlight"
-            onClick={store.backspace}
-            id="backspace"
-          >
-            C
-          </button>
-          {buttons.map((item, index) => (
-            <button
-              key={index}
-              className={`highlight__${item.classes}`}
-              value={item.value}
-              onClick={store.handleClick}
-            >
-              {item.display}
-            </button>
-          ))}
-          <button className="highlight" onClick={store.calculate} id="result">
-            =
-          </button>
-        </div>
-      </div>
-    </>
-  )
+const App = () => {
+  return <Form />
 }
 
-export default inject('store')(observer(App))
+export default App
